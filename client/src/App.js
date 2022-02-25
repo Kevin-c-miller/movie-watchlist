0fd069a2f9c13a92e3d21f11106acb217137c926
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-02-25 10:05:38 -0500

    created react app, installed axios and react router, deleted boilerplate react code.

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { Routes, Router } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default App;
