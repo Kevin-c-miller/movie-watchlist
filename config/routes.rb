97bdc76d77bf0baa2f2c740b424da9718cce5373
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-02-24 19:20:22 -0500

    login route is working

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
   end
 
   get '/users/:user_id/movies', to: 'movies#get_user_movies'
-  get '/reviews', to: 'reviews#get_all_reviews'
+  # get '/reviews', to: 'reviews#get_all_reviews'
 
-  post '/auth/login', to: 'authentications#login'
-  get '/auth/verify', to: 'authentications#verify'
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
