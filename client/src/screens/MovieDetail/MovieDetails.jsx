1f58bba0fe01d66291aaa093b478afe1bd8592e9
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-02-27 17:28:43 -0500

    styled all movies page

--- client/src/screens/MovieDetail/MovieDetails.jsx
+++ client/src/screens/MovieDetail/MovieDetails.jsx
@@ -8,60 +8,69 @@ export default function MovieDetails(props) {
 
   return (
     <div className="movie-details">
-      <h2>{movie.Title}</h2>
-      <div className="movie-poster">
-        <img src={movie.Poster} alt={movie.Title} />
-      </div>
-      <h4>Director: {movie.Director}</h4>
-      <h5>Starring: {movie.Actors}</h5>
-      <h6>
-        <b>Rated:</b> {movie.Rated}
-      </h6>
-      <h6>
-        <b>Released:</b> {movie.Released}
-      </h6>
-      <h6>
-        <b>Runtime: </b>
-        {movie.Runtime}
-      </h6>
-      <h6>
-        <b>Genre:</b> {movie.Genre}
-      </h6>
-      <h6>
-        <b>Box Office:</b> {movie.BoxOffice}
-      </h6>
-      <div className="plot">
-        <p>
-          <b>Plot:</b> {movie.Plot}
-        </p>
-      </div>
-      <h6>
-        <b>Written By:</b> {movie.Writer}
-      </h6>
-      <h6>
-        <b>Awards:</b> {movie.Awards}
-      </h6>
+      <div className="movie-container">
+        <h2>{movie.Title}</h2>
+        <div className="movie-poster">
+          <img src={movie.Poster} alt={movie.Title} />
+        </div>
+        <div className="movie-info">
+          <h5>
+            <b>Director:</b> {movie.Director}
+          </h5>
+          <h6>
+            <b>Starring:</b> {movie.Actors}
+          </h6>
+          <h6>
+            <b>Rated:</b> {movie.Rated}
+          </h6>
+          <h6>
+            <b>Released:</b> {movie.Released}
+          </h6>
+          <h6>
+            <b>Runtime: </b>
+            {movie.Runtime}
+          </h6>
+          <h6>
+            <b>Genre:</b> {movie.Genre}
+          </h6>
+          <h6>
+            <b>Box Office:</b> {movie.BoxOffice}
+          </h6>
+          <div className="plot">
+            <p>
+              <b>Plot:</b> {movie.Plot}
+            </p>
+          </div>
+          <h6>
+            <b>Written By:</b> {movie.Writer}
+          </h6>
+          <h6>
+            <b>Awards:</b> {movie.Awards}
+          </h6>
+        </div>
 
-      {props.currentUser && (
-        <button
-          onClick={() => {
-            const addedMovie = {
-              title: movie.Title,
-              poster: movie.Poster,
-              rating: movie.Rated,
-              synopsis: movie.Plot,
-              director: movie.Director,
-              starring: movie.Actors,
-              release_year: parseInt(movie.Year),
-              runtime: movie.Runtime,
-              user_id: props.currentUser.id,
-            };
-            props.addMovieToWatchList(props.currentUser.id, addedMovie);
-          }}
-        >
-          Add Movie to watch list
-        </button>
-      )}
+        {props.currentUser && (
+          <button
+            className="add-movie-btn"
+            onClick={() => {
+              const addedMovie = {
+                title: movie.Title,
+                poster: movie.Poster,
+                rating: movie.Rated,
+                synopsis: movie.Plot,
+                director: movie.Director,
+                starring: movie.Actors,
+                release_year: parseInt(movie.Year),
+                runtime: movie.Runtime,
+                user_id: props.currentUser.id,
+              };
+              props.addMovieToWatchList(props.currentUser.id, addedMovie);
+            }}
+          >
+            Add Movie to watch list
+          </button>
+        )}
+      </div>
     </div>
   );
 }
