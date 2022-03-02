a2aef00fd8039891956d1c86cb80f30f82e32854
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-03-02 15:19:01 -0500

    deployed to heroku and added heroku link to apiconfig

--- client/src/services/apiConfig/index.js
+++ client/src/services/apiConfig/index.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3000';
-// const baseUrl = process.env.NODE_ENV === 'production' ? /* link to your heroku app. Example:*/'https://school-app-test.herokuapp.com/' : 'http://localhost:3000'
+// const baseURL = 'http://localhost:3000';
+const baseUrl =
+  process.env.NODE_ENV === 'production'
+    ? /* link to your heroku app. Example:*/ 'https://peaceful-shore-90420.herokuapp.com'
+    : 'http://localhost:3000';
 
 export const api = axios.create({
-  baseURL: baseURL,
+  baseURL: baseUrl,
 });
