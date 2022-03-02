9fa4fdf4b12d8b47f549cb63da51dc8c296c2698
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-03-02 10:59:39 -0500

    removed console.logs from code

--- client/src/services/apiConfig/index.js
+++ client/src/services/apiConfig/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const baseURL = 'http://localhost:3000';
+// const baseUrl = process.env.NODE_ENV === 'production' ? /* link to your heroku app. Example:*/'https://school-app-test.herokuapp.com/' : 'http://localhost:3000'
 
 export const api = axios.create({
   baseURL: baseURL,
