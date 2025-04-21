78ebd10b83f1033fd4cfbe7aa5d91ccd19c4d04d
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2025-04-21 17:27:06 -0400

    removed collapsable navbar since it is not needed

--- client/src/components/Navbar/Nav.jsx
+++ client/src/components/Navbar/Nav.jsx
@@ -3,28 +3,24 @@ import { movieticket } from '../../assets/index.js';
 import './Nav.css';
 
 export default function Nav(props) {
-  return (
-    <div>
-      <ReactBootStrap.Navbar
-        collapseOnSelect
-        expand="xl"
-        bg="dark"
-        variant="dark"
-        sticky="top"
-      >
-        <ReactBootStrap.Container>
-          <ReactBootStrap.Navbar.Brand href="/" className="Nav-header">
-            <span className="navbar-header">
-              <img src={movieticket} alt="movie ticket icon" />
-              Movie Watch-List
-            </span>
-          </ReactBootStrap.Navbar.Brand>
-          <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-            <ReactBootStrap.Nav className="me-auto"></ReactBootStrap.Nav>
-          </ReactBootStrap.Navbar.Collapse>
-        </ReactBootStrap.Container>
-      </ReactBootStrap.Navbar>
-    </div>
-  );
+	return (
+		<div>
+			<ReactBootStrap.Navbar
+				collapseOnSelect
+				expand="xl"
+				bg="dark"
+				variant="dark"
+				sticky="top"
+			>
+				<ReactBootStrap.Container>
+					<ReactBootStrap.Navbar.Brand href="/" className="Nav-header">
+						<span className="navbar-header">
+							<img src={movieticket} alt="movie ticket icon" />
+							Movie Watch-List
+						</span>
+					</ReactBootStrap.Navbar.Brand>
+				</ReactBootStrap.Container>
+			</ReactBootStrap.Navbar>
+		</div>
+	);
 }
